0e5d32bdea9af8b983c8583a6ca66d4201b9e0e9
Tremho <steve@ohmert.com>
2021-11-20 10:24:56 -0800

    test report trace saturday

--- index.d.ts
+++ index.d.ts
@@ -9,14 +9,14 @@ declare module "@tremho/jove-common" {
      *
      * @return {boolean} the result of the test result matching expected; may be ignored.
      */
-    export declare function testRemote(t: any, action: string, description: string, expected: any): Promise<boolean>;
+    export function testRemote(t: any, action: string, description: string, expected: any): Promise<boolean>;
     /**
      * similar to `testRemote`, but simply calls the action and returns the result without submitting to test
      *
      * @param action The directive to perform
      * @returns {string} The JSON result of the action, Stringified
      */
-    export declare function callRemote(action: string): Promise<unknown>;
+    export function callRemote(action: string): Promise<unknown>;
     /**
      * Should be called at the top of a test suite, but it's just for symmetry with `endTest`
      * In this version, nothing is actually sent to the server.
@@ -24,13 +24,13 @@ declare module "@tremho/jove-common" {
      *
      * return {boolean} true if stream is ready.
      */
-    export declare function startTest(t?: any): Promise<boolean>;
+    export function startTest(t?: any): Promise<boolean>;
     /**
      * Call when all the tests are complete.  The client will continue with its disposition after that, either exiting, or
      * continuing to run.  This should be the end of any remote testing, regardless.
      * @param t The tap instance, if using tap.  Will signal the end on this tap instance.
      */
-    export declare function endTest(t?: any): Promise<void>;
+    export function endTest(t?: any): Promise<void>;
     /**
      * Initiate the connected test.  Pass the title of the test and the async function that conducts the test suite
      *
@@ -39,5 +39,5 @@ declare module "@tremho/jove-common" {
      * @param title Title of this test that will appear on the report
      * @param testFunc The function from the test script that conducts the test with `startTest` then a series of `testRemote` directives, then an `endTest`
      */
-    export declare function runRemoteTest(title: string, testFunc: any): Promise<void>;
+    export function runRemoteTest(title: string, testFunc: any): Promise<void>;
 }
