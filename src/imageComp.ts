f697eea5b3b23e65283ff4e7efa65da99053bc34
Tremho <steve@ohmert.com>
2021-11-24 11:50:18 -0800

    image compare mobile

--- src/imageComp.ts
+++ src/imageComp.ts
@@ -8,29 +8,37 @@ export function compareImages(imgPath1:string, imgPath2:string, passingPct:numbe
     console.log('-->CompareImages ', imgPath1, imgPath2)
     return new Promise(resolve => {
 
-        const img1 = PNG.sync.read(fs.readFileSync(imgPath1));
-        const img2 = PNG.sync.read(fs.readFileSync(imgPath2));
-        const {width, height} = img1;
-        const diff = new PNG({width, height});
-        console.log('images', img1, img2, diff)
-        const delta = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
-        console.log('delta', delta)
-        const diffPath = imgPath1.substring(0, imgPath1.lastIndexOf('.'))+'-diff.png'
-        fs.writeFileSync(diffPath, PNG.sync.write(diff));
+        let data:any = {}
 
-        let tpix = width*height
-        let pct:any = 100*delta/tpix
-        let ok = pct <= passingPct
-        pct = ''+pct
-        pct = pct.substring(0,8)
-        console.log(`${delta} out of ${tpix} pixels differ (${pct}%) in ${width}x${height} image. okay=${ok}`)
-        const data = {
-            ok,
-            width,
-            height,
-            countDiff: delta,
-            percentDiff: pct,
-            diffPath
+        try {
+            const img1 = PNG.sync.read(fs.readFileSync(imgPath1));
+            const img2 = PNG.sync.read(fs.readFileSync(imgPath2));
+            const {width, height} = img1;
+            const diff = new PNG({width, height});
+            console.log('images', img1, img2, diff)
+            const delta = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+            console.log('delta', delta)
+            const diffPath = imgPath1.substring(0, imgPath1.lastIndexOf('.')) + '-diff.png'
+            fs.writeFileSync(diffPath, PNG.sync.write(diff));
+
+            let tpix = width * height
+            let pct: any = 100 * delta / tpix
+            let ok = pct <= passingPct
+            pct = '' + pct
+            pct = pct.substring(0, 8)
+            console.log(`${delta} out of ${tpix} pixels differ (${pct}%) in ${width}x${height} image. okay=${ok}`)
+            data = {
+                ok,
+                width,
+                height,
+                countDiff: delta,
+                percentDiff: pct,
+                diffPath
+            }
+        }
+        catch(e) {
+            console.error('error in compare', e)
+            data.error = e.toString()
         }
 
         resolve(data)
